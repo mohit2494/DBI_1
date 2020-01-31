2409c005c200650a0b46afa1628e9b277e7f61dc
Mohit Kalra <mohit2494@gmail.com>
2020-01-31 02:06:22 -0500

    Added GTests Final

--- myGtest.cc
+++ myGtest.cc
@@ -13,39 +13,66 @@ class FilePath{
 };
 
 
-// creating DBFile at specified location
+
 TEST(DBFileTesting, Create) {
-    // creating DBFile for nation table
+    
+    // scenario 1: Create a file
     FilePath filepath;
     const string schemaSuffix = "nation.bin";
-    const string completePath = filepath.dbfile_dir + schemaSuffix;
+    const string dbFilePath = filepath.dbfile_dir + schemaSuffix;
     DBFile dbfile;
-    ASSERT_EQ(1,dbfile.Create(completePath.c_str(),heap,NULL));
-    
-    // loading file so that open can use it
-    Schema nation ("catalog", "nation");
-    dbfile.Load(nation, completePath.c_str());
+    ASSERT_EQ(1,dbfile.Create(dbFilePath.c_str(),heap,NULL));
+    // Schema nation ("catalog", "nation");
+    // dbfile.Load(nation, dbFilePath.c_str());
+    dbfile.Close();
+
+    // scenario 2: Creating a file which already exists
+    // creating DBFile for nation table
+    ASSERT_EQ(0,dbfile.Create(dbFilePath.c_str(),heap,NULL));
+
 }
+
 
 TEST(DBFileTesting, Open) {
 
     // creating DBFile for nation table
     FilePath filepath;
-    const string schemaSuffix = "nation.bin";
-    const string completePath = filepath.dbfile_dir + schemaSuffix;
     DBFile dbfile;
-    ASSERT_EQ(1,dbfile.Open(completePath.c_str()));
+    const string existentFile = "nation.bin";
+    const string nonExistentFile = "something.bin";
+    const string existentPath = filepath.dbfile_dir + existentFile;
+    const string nonExistentPath = filepath.dbfile_dir + nonExistentFile;
+    
+    // scenario 2
+    // Trying to open a file which exists
+    ASSERT_EQ(1,dbfile.Open(existentPath.c_str()));
+    
+    // scenario 3
+    // Trying to open a file which is already open
+    ASSERT_EQ(1,dbfile.Open(existentPath.c_str()));
+    dbfile.Close();
+
+    // scenario 1:
+    // Trying to open a file which doesn't exist
+    ASSERT_EQ(0,dbfile.Open(nonExistentPath.c_str()));
+
+    // close file after test cases have run
+    dbfile.Close();
 }
  
 
-TEST(DBFileTesting, Close) { 
-    // creating DBFile for nation table
+TEST(DBFileTesting, Close) {
+
+    // scenario 1: trying to close a file which is not open
     FilePath filepath;
     const string schemaSuffix = "nation.bin";
     const string completePath = filepath.dbfile_dir + schemaSuffix;
     DBFile dbfile;
+    ASSERT_EQ(0,dbfile.Close());
+
+    // // scenario 2: trying to close an open file
     dbfile.Open(completePath.c_str());
-    ASSERT_EQ(1,dbfile.Close());
+    ASSERT_EQ(1,dbfile.Close());    
 }
 
 int main(int argc, char **argv) {
