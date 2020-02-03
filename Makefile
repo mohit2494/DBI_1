671aca0b08faa61eeb8956beb895178473de7955
Mohit Kalra <mohit2494@gmail.com>
2020-02-03 02:26:04 -0500

    Fixed gtest for local directory

--- Makefile
+++ Makefile
@@ -6,17 +6,60 @@ ifdef linux
 tag = -n
 endif
 
+######################################################################
+GTEST_DIR = googletest/googletest
+
+# Flags passed to the preprocessor.
+# Set Google Test's header directory as a system directory, such that
+# the compiler doesn't generate warnings in Google Test headers.
+CPPFLAGS += -isystem $(GTEST_DIR)/include
+
+# Flags passed to the C++ compiler.
+CXXFLAGS += -g -Wall -Wextra -pthread
+
+# All Google Test headers.  Usually you shouldn't change this
+# definition.
+GTEST_HEADERS = $(GTEST_DIR)/include/gtest/*.h \
+                $(GTEST_DIR)/include/gtest/internal/*.h
+
+# Builds gtest.a and gtest_main.a.
+
+# Usually you shouldn't tweak such internal variables, indicated by a
+# trailing _.
+GTEST_SRCS_ = $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
+
+# For simplicity and to avoid depending on Google Test's
+# implementation details, the dependencies specified below are
+# conservative and not optimized.  This is fine as Google Test
+# compiles fast and for ordinary users its source rarely changes.
+gtest-all.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest-all.cc -o gtest-all.o
+
+gtest_main.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest_main.cc -o gtest_main.o
+
+gtest.a : gtest-all.o
+	$(AR) $(ARFLAGS) $@ $^
+
+gtest_main.a : gtest-all.o gtest_main.o
+	$(AR) $(ARFLAGS) $@ gtest-all.o gtest_main.o
+
+######################################################################
+
+
+gtest.o: gtest.cc
+	$(CC) -g -c gtest.cc
+
+gtest: Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o gtest-all.o gtest.o
+	$(CC) -o gtest Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o gtest-all.o gtest.o -lfl -lpthread -lgtest
+
 test.out: Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o test.o
 	$(CC) -o test.out Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o test.o -lfl
 	
 main: Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o	y.tab.o lex.yy.o main.o
 	$(CC) -o main Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o	y.tab.o lex.yy.o main.o -lfl
-
-myGtest.out: Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o myGtest.o
-	$(CC) -o myGtest.out Record.o Comparison.o ComparisonEngine.o Schema.o File.o DBFile.o y.tab.o lex.yy.o myGtest.o -lfl -lpthread -lgtest
-
-myGtest.o: myGtest.cc
-	$(CC) -g -c myGtest.cc
 
 test.o: test.cc
 	$(CC) -g -c test.cc
--- gtest.cc
+++ gtest.cc
@@ -7,14 +7,18 @@
 class FilePath{
     public:
     // make sure that the file path/dir information below is correct
-    const std::string dbfile_dir = "/home/mk/Documents/uf docs/sem 2/Database Implementation/DBI/dbfiles/"; // dir where binary heap files should be stored
-    const std::string tpch_dir ="/home/mk/Documents/uf docs/sem 2/Database Implementation/git/tpch-dbgen/"; // dir where dbgen tpch files (extension *.tbl) can be found
+    // dir where binary heap files should be stored
+    const std::string dbfile_dir = "dbfiles/"; 
+    
+    //****************** update the following path as per requirement*************************************/
+    // dir where dbgen tpch files (extension *.tbl) can be found
+    const std::string tpch_dir ="/home/mk/Documents/uf docs/sem 2/Database Implementation/git/tpch-dbgen/"; 
+
     const std::string catalog_path = "catalog"; // full path of the catalog file
 };
 
 
-
-TEST(DBFileTesting, Create) {
+TEST(DBFileTestingCreate, Scenario1_Create) {
     
     // scenario 1: Create a file
     FilePath filepath;
@@ -26,14 +30,37 @@ TEST(DBFileTesting, Create) {
     // dbfile.Load(nation, dbFilePath.c_str());
     dbfile.Close();
 
+}
+
+TEST(DBFileTestingCreate, Scenario2_Create) {
+    
+    FilePath filepath;
+    const string schemaSuffix = "nation.bin";
+    const string dbFilePath = filepath.dbfile_dir + schemaSuffix;
+    DBFile dbfile;
+   
     // scenario 2: Creating a file which already exists
     // creating DBFile for nation table
     ASSERT_EQ(0,dbfile.Create(dbFilePath.c_str(),heap,NULL));
+}
+
+TEST(DBFileTesting, Scenario1_Open) {
+
+    // creating DBFile for nation table
+    FilePath filepath;
+    DBFile dbfile;
+    const string existentFile = "nation.bin";
+    const string nonExistentFile = "something.bin";
+    const string existentPath = filepath.dbfile_dir + existentFile;
+    const string nonExistentPath = filepath.dbfile_dir + nonExistentFile;
+    
+    // scenario 1:
+    // Trying to open a file which doesn't exist
+    ASSERT_EQ(0,dbfile.Open(nonExistentPath.c_str()));
 
 }
 
-
-TEST(DBFileTesting, Open) {
+TEST(DBFileTesting, Scenario2_Open) {
 
     // creating DBFile for nation table
     FilePath filepath;
@@ -46,20 +73,26 @@ TEST(DBFileTesting, Open) {
     // scenario 2
     // Trying to open a file which exists
     ASSERT_EQ(1,dbfile.Open(existentPath.c_str()));
-    
+
+}
+
+TEST(DBFileTesting, Scenario3_Open) {
+
+    // creating DBFile for nation table
+    FilePath filepath;
+    DBFile dbfile;
+    const string existentFile = "nation.bin";
+    const string nonExistentFile = "something.bin";
+    const string existentPath = filepath.dbfile_dir + existentFile;
+    const string nonExistentPath = filepath.dbfile_dir + nonExistentFile;
+        
     // scenario 3
     // Trying to open a file which is already open
     ASSERT_EQ(1,dbfile.Open(existentPath.c_str()));
-    dbfile.Close();
-
-    // scenario 1:
-    // Trying to open a file which doesn't exist
-    ASSERT_EQ(0,dbfile.Open(nonExistentPath.c_str()));
-
+    
     // close file after test cases have run
     dbfile.Close();
 }
- 
 
 TEST(DBFileTesting, Close) {
 
